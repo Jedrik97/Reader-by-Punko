edd1ebab48908fa5f89487ecb335a68c9b94e3c7
Jedrik97 <punick97@gmail.com>
2025-10-16 14:38:10 +0300

    Revert "update"
    This reverts commit 72fbfdb9b7d0752b0e776e9677cb41d8aaa8da4d.

--- PuReader/Assets/Scripts/IPdfRenderer.cs
+++
@@ -1,8 +0,0 @@
-public interface IPdfRenderer : System.IDisposable
-{
-    int PageCount { get; }
-    // Вернуть размер страницы в пунктах (pt) или пикселях — выбираем пиксели при базовом DPI
-    (int width, int height) GetPageSizePx(int pageIndex, float dpi = 150f);
-    // Синхронный или асинхронный рендер в Texture2D-compatible byte[]
-    byte[] RenderPageRGBA(int pageIndex, int targetWidth, int targetHeight, float dpi = 150f);
-}
